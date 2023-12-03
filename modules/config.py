bf0961c0ad254dbb7364eb18e0fd9c7cb6c178fb
Hbaicbke <151785171+Hbaicbke@users.noreply.github.com>
2023-12-04 04:15:10 +0530

    Update config.py
    Signed-off-by: Hbaicbke <151785171+Hbaicbke@users.noreply.github.com>

--- modules/config.py
+++ modules/config.py
@@ -8,10 +8,10 @@ que = {}
 admins = {}
 aiohttpsession = aiohttp.ClientSession()
 
-API_ID = int(getenv("API_ID", "26030478"))
-API_HASH = getenv("API_HASH", "176fa1a0a1316f8517dbf941d8bdf5ef")
-BOT_TOKEN = getenv("BOT_TOKEN", "6569449035:AAGNN6l7c6F41wSipOlIW6Ul5b2bX1s09KY")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "2400"))
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "300"))
 STRING_SESSION = getenv("STRING_SESSION", "")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1282754256").split()))
